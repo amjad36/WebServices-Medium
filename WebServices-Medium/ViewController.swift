4b4041bf018833093fc55af505f8eef360e6229e
“Amjad <amjad.k@hcl.com>
2017-12-19 13:14:35 +0530

    Initial Commit

---
+++ WebServices-Medium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebServices-Medium
+//
+//  Created by Amjad Khan on 19/12/17.
+//  Copyright © 2017 HCL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
